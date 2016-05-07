11b587945954fe32e4c517e975220cf33c62c1c6
ShenglanQiao <shenglan@stanford.edu>
2016-05-06 18:00:02 -0700

    predict funtion

--- src/dnaAnalysis.py
+++ src/dnaAnalysis.py
@@ -15,21 +15,21 @@ def pull_outliers(data,m):
     return data[s<m],data[s>m]
 
 # this should be a debug function
-def make_histograms(data,sig_cutoff):
+def make_histograms(data,sig_cutoff,num_bins):
 
 #   separate data by outliers (side) and not outliers (center)
     center,side=pull_outliers(data,sig_cutoff)
     
 #   make two histograms
     bin_range=(np.min(side),np.max(side))
-    n_center,bins_center=np.histogram(center,bins=150,range=bin_range)
-    n_side,bins_side=np.histogram(side,bins=150,range=bin_range)
+    n_center,bins_center=np.histogram(center,bins=num_bins,range=bin_range)
+    n_side,bins_side=np.histogram(side,bins=num_bins,range=bin_range)
     
     return bins_center,n_center,bins_side,n_side
     
-def train(data, sig_cutoff):
+def train(data, sig_cutoff,num_bins):
     
-    bins_center,n_center,bins_side,n_side = make_histograms(data,sig_cutoff)
+    bins_center,n_center,bins_side,n_side = make_histograms(data,sig_cutoff,num_bins)
     
 #   get the x-axis for gaussian fit 
     side_x=bins_side[n_side>0]
@@ -39,18 +39,40 @@ def train(data, sig_cutoff):
     try:
         coefs_side,_=curve_fit(gaussian,side_x,n_side[n_side>0])
     except RuntimeError:
-        utilities.printWarnMsg("One failure in gaussian fit")
-        return
+        utilities.printWarningMsg("One failure in gaussian fit")
+        return np.zeros((2,3))
         
 #   compute residual by interpolating fit to outliers . Fit residuals
     g_left=n_center[n_center>0]-gaussian(center_x,*coefs_side)
     try:
         coefs_center,_=curve_fit(gaussian,center_x,g_left)
     except RuntimeError:
-        utilities.printWarnMsg("One failure in gaussian fit")
-        return
+        utilities.printWarningMsg("One failure in gaussian fit")
+        return np.zeros((2,3))
     
     return np.array([coefs_center,coefs_side])
+
+def predict(data,model):
+    try:
+        assert data.shape[0] == model.shape[0] 
+    except AssertionError:
+        utilities.printErrorMsg("Model and data shape mismatch")
+        return 0., 0. 
+    
+    p_c = 0.
+    p_u = 0.
+    
+    for i in range(data.shape[0]):
+        n_c = normal(data[i],model[i][0][0],model[i][0][1])
+        n_u = normal(data[i],model[i][1][0],model[i][1][1])
+        
+        p_c += n_c/(n_c+n_u)
+        p_u += n_u/(n_c+n_u)
+    
+    p_c=p_c/data.shape[0]
+    p_u=p_c/data.shape[0]
+    
+    return p_c,p_u
 
 def reduce():
     return
@@ -58,6 +80,5 @@ def reduce():
 def transfrom():
     return
 
-def predict():
-    return
+
     
